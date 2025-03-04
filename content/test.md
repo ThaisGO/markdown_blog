1f4a7c68332a0e63443790f7e8351d8c91100feb
ThaisGO <thaisgo.viana@gmail.com>
2025-03-04 17:33:54 -0300

    fix: fixed nuxt/content version

---
+++ content/test.md
@@ -0,0 +1,6 @@
+---
+title: Teste de Post
+description: Isso é um post de teste.
+---
+
+Esse é um conteúdo de teste.
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,7 @@
 <template>
     <div>
         <ContentList path="/posts" v-slot="{ list }">
-            <!-- {{ list }} -->
-              list
+            {{ list }}
         </ContentList> 
     </div>
 </template>
